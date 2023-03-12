fdda1187aea7a2714d044a9e05e71c398a07625b
erictu22 <erictu222@gmail.com>
2023-03-12 15:52:23 -0400

    Progress is displayed while videos are downloading

--- extract_videos.py
+++ extract_videos.py
@@ -4,10 +4,11 @@ import twitchdl
 from pprint import pprint
 from datetime import datetime
 import subprocess
+import sys
 
 # TODO: Delete irrelevant videos from /videos
 # To-add: Lourlo
-VIDEO_AGE_THRESHOLD = 1 #weeks
+VIDEO_AGE_THRESHOLD = 1 # days
 TOP_LANE_STREAMERS = ['Sanchovies' ,'yung_fappy','Dragoon','Lourlo','Thebausffs','Bwipolol','foggedftw2','SoloRenektonOnly']
 TEST_STREAMERS = ['yung_fappy']
 def is_video_relevant(video):
@@ -27,7 +28,6 @@ def get_relevant_videos(channel_names):
 def get_local_videos():
     videos = []
     for file_name in os.listdir('videos'):
-
         skip_values = ['.DS_Store']
         if file_name in skip_values:
             continue
@@ -41,9 +41,22 @@ def get_local_videos():
     return videos
 
 def download_videos(videos):
+    if len(videos) == 0:
+        print('No relevant videos to download')
+        return
+
     video_ids = [video['id'] for video in videos]
     id_str = ' '.join(video_ids)
-    p = subprocess.Popen(f'cd videos && twitch-dl download -q source -f mp4 -o {"{id}.mp4"} {id_str}'.split(), stdout=subprocess.PIPE)
+    print(f'Downloading {id_str}')
+    cmd = f'twitch-dl download -q source -f mp4 -o videos/{"{id}.mp4"} {id_str}'
+    with subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, text=True, bufsize=1) as p:
+        for line in p.stdout:
+            print('\r' + str(line).replace('\n',''), end='')
+
+    if p.returncode != 0:
+        raise subprocess.CalledProcessError(p.returncode, p.args)
+    print('\r')
+    p.kill()
 
 # TODO: Save video metadata
 
@@ -56,5 +69,5 @@ def get_diff():
     return videos_to_delete, videos_to_download
 
 delete, download = get_diff()
-print(download)
-# download_videos(download)
+download_videos(download)
+print('Done')
